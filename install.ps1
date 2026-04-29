45b8ce0cc589b00cf5892a107642cb964038877b
kushalhk26 <premakotresh2003@gmail.com>
2026-04-29 15:57:45 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,10 @@
 Write-Host "Downloading project..."
 
 if (!(Test-Path "pec-oec-auto-allocator")) {
-    git clone https://github.com/YOUR_USERNAME/pec-oec-auto-allocator.git
+    git clone https://github.com/kushalhk26/pec-oec-auto-allocator.git
 }
 
 cd pec-oec-auto-allocator
-
-Write-Host "Installing dependencies..."
-pip install -r requirements.txt
 
 Write-Host "Running app..."
 python app.py
